0323d08470e2f0edd3a0207a8811c7a3528e8041
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2015-02-05 21:53:44 +0100

    Fix typos in scripts
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

--- bash/50_prompt.sh
+++ bash/50_prompt.sh
@@ -3,6 +3,6 @@
 # On OSX, source Liquibase from homebrew
 if is_osx && [ -f $(brew --prefix)/share/liquidprompt ]; then
   . $(brew --prefix)/share/liquidprompt
-elif [ -f ~/liquidprompt ]; 
+elif [ -f ~/liquidprompt ]; then
   . ~/liquidprompt
 fi
