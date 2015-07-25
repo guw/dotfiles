56380990f708aff4dde0d5c5b6e50cdf6da26139
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2015-07-25 10:41:58 -0700

    Script for installing/updating liquidprompt

---
+++ init/30_ubuntu_liquibase.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Ubuntu-only stuff. Abort if not Ubuntu.
+is_ubuntu || return 1
+
+# Install / Update Liquibase
+LIQUIDPROMPT= ~/.liquidprompt
+if [[ ! -d $LIQUIDPROMPT ]]; then
+  # $LIQUIDPROMPT directory doesn't exist? Clone it!
+  e_header "Downloading liquidprompt"
+  git clone --recursive git://github.com/nojhan/liquidprompt.git $LIQUIDPROMPT
+else
+  # Make sure we have the latest files.
+  e_header "Updating liquidprompt"
+  pushd $LIQUIDPROMPT
+  git pull
+  git submodule update --init --recursive --quiet
+  popd
+fi
