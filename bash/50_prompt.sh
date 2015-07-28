1fec7cb78a391d50d5b66c3868f80d4d089391d0
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2015-07-27 21:53:28 -0700

    update iTerm title based on current dir

--- bash/50_prompt.sh
+++ bash/50_prompt.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# set iTerm tab title to current working directory
+if is_ssh; then
+  PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}: $(basename $PWD)\007"'
+else
+	PROMPT_COMMAND='echo -ne "\033]0;$(basename $PWD)\007"'
+fi
+
 # On OSX, source Liquibase from homebrew
 if is_osx && [ -f $(brew --prefix)/share/liquidprompt ]; then
   . $(brew --prefix)/share/liquidprompt
@@ -11,3 +18,4 @@ fi
 
 # Something for orientation
 alias cls='clear;echo "Logged in at $(tty), as $(whoami) in $(pwd)."'
+
