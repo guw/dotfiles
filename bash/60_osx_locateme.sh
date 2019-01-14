4084c0df22cf7d61ab86a8cddde295a7ce2bb120
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2019-01-14 21:58:45 +0100

    Remove obsolete stuff

--- bash/60_osx_locateme.sh
+++ bash/60_osx_locateme.sh
@@ -11,14 +11,19 @@ if [ $? -eq 0 ]; then
   latitude=$(echo "${location}" | awk '/Latitude: / {print $2}')
   longitude=$(echo "${location}" | awk '/Longitude: / {print $2}')
   response=$(curl --connect-timeout 3 --max-time 5 -s "http://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&sensor=false")
-        locality=$(echo ${response} | jq -r '.results | .[0] | .address_components[] | select(.types==["locality","political"]) | .long_name')
-        locality_short=$(echo ${response} | jq -r '.results | .[0] | .address_components[] | select(.types==["locality","political"]) | .short_name')
-        state=$(echo ${response} | jq -r '.results | .[0] | .address_components[] | select(.types==["administrative_area_level_1","political"]) | .short_name')
-        country=$(echo ${response} | jq -r '.results | .[0] | .address_components[] | select(.types==["country","political"]) | .short_name')
-        #echo -e "Welcome to \e[1m${locality}\e[0m."
+	if [ $? -ne 0 ]; then
+	  error_message=$(echo ${response} | jq -r '.error_message ')
+	  echo -e "Google Maps API failed: ${error_message}"
+	  return 1
+	fi
+  locality=$(echo ${response} | jq -r '.results | .[0] | .address_components[] | select(.types==["locality","political"]) | .long_name')
+  locality_short=$(echo ${response} | jq -r '.results | .[0] | .address_components[] | select(.types==["locality","political"]) | .short_name')
+  state=$(echo ${response} | jq -r '.results | .[0] | .address_components[] | select(.types==["administrative_area_level_1","political"]) | .short_name')
+  country=$(echo ${response} | jq -r '.results | .[0] | .address_components[] | select(.types==["country","political"]) | .short_name')
+  echo -e "Welcome to \e[1m${locality}\e[0m."
 
-        export LOCATION_LOCALITY="${locality}"
-        export LOCATION_LOCALITY_SHORT="${locality_short}"
-        export LOCATION_STATE="${state}"
-        export LOCATION_COUNTRY="${country}"
+  export LOCATION_LOCALITY="${locality}"
+  export LOCATION_LOCALITY_SHORT="${locality_short}"
+  export LOCATION_STATE="${state}"
+  export LOCATION_COUNTRY="${country}"
 fi
--- init/30_osx_homebrew_casks.sh
+++ init/30_osx_homebrew_casks.sh
@@ -6,15 +6,6 @@ is_osx || return 1
 # Exit if Homebrew is not installed.
 [[ ! "$(type -P brew)" ]] && e_error "Brew casks need Homebrew to install." && return 1
 
-# Ensure the cask keg and recipe are installed.
-kegs=(caskroom/cask caskroom/versions)
-brew_tap_kegs
-recipes=(brew-cask)
-brew_install_recipes
-
-# Exit if, for some reason, cask is not installed.
-[[ ! "$(brew ls --versions brew-cask)" ]] && e_error "Brew-cask failed to install." && return 1
-
 # Hack to show the first-run brew-cask password prompt immediately.
 brew cask info this-is-somewhat-annoying 2>/dev/null
 
@@ -26,33 +17,18 @@ casks=(
   burn
   chronosync
   cloudytabs
-  commander-one
+  #commander-one
   #cyberduck
-  #dockertoolbox  ## -> Docker.app Mac beta
-  #google-chrome ## -> need to install manuall, otherwise 1 password ext fails
-  google-drive
   iterm2
   java
   yujitach-menumeters
   omnidisksweeper
-  sequel-pro
-  skype
-  sourcetree
+  #sequel-pro
+  #sourcetree
   sublime-text3
   vagrant
   virtualbox
   virtualbox-extension-pack
-  # Quick Look plugins
-  betterzipql
-  qlcolorcode
-  qlmarkdown
-  qlprettypatch
-  qlstephen
-  quicklook-csv
-  quicklook-json
-  quicknfo
-  suspicious-package
-  webpquicklook
 )
 
 # Install Homebrew casks.
