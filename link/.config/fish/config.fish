7f65fa9f874a9391bdefdf19e94abab3284ef466
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2020-12-18 20:02:59 +0100

    NVM in Fish

--- link/.config/fish/config.fish
+++ link/.config/fish/config.fish
@@ -6,3 +6,11 @@ if command -v starship > /dev/null
 else
     echo "Starship prompt (https://starship.rs/) isn't installed. Check dotfiles setup."
 end
+
+# NVM (https://medium.com/@joshuacrass/nvm-on-mac-for-fish-users-e00af124c540)
+function nvm
+   bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
+end
+
+set -x NVM_DIR ~/.nvm
+nvm use default --silent
