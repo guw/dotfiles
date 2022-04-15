adc39ac4d29cf6945c7a09d44b43195b6e57f349
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2022-04-15 12:16:28 +0200

    Don't run install as root

--- init/20_osx_homebrew.sh
+++ init/20_osx_homebrew.sh
@@ -6,7 +6,7 @@ is_osx || return 1
 # Install Homebrew.
 if [[ ! "$(type -P brew)" ]]; then
   e_header "Installing Homebrew"
-  sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   if [[ -x "/opt/homebrew/bin/brew" ]]; then
     eval $(/opt/homebrew/bin/brew shellenv)
   elif [[ -x "/usr/local/bin/brew" ]]; then
